45db0ac66f6bc149f26c4c6e7ff85b041b2098b2
architraj99 <rajarchit99@gmail.com>
2026-04-26 10:40:21 +0530

    Added detailed result summary and history section
    improved result display with detailed info
    
    added history section and made small UI improvements

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function getSelectedText(selectId) {
+    let select = document.getElementById(selectId);
+    return select.options[select.selectedIndex].text;
+}
+
 function convertTime() {
     let fromZone = document.getElementById("fromZone").value;
     let toZone = document.getElementById("toZone").value;
@@ -5,7 +10,7 @@ function convertTime() {
     let time = document.getElementById("time").value;
 
     if (date === "" || time === "") {
-        document.getElementById("result").innerText = "Please Select Date And Time";
+        document.getElementById("resultMessage").innerText = "Please Select Date And Time";
         return;
     }
 
@@ -13,7 +18,20 @@ function convertTime() {
 
     let fromDate = new Date(dateTimeString);
 
-    let converted = fromDate.toLocaleString("en-US", {timeZone: toZone});
+    let converted = fromDate.toLocaleString("en-US", { 
+        timeZone: toZone,
+        dateStyle: "medium",
+        timeStyle: "short"
+    });
 
-    document.getElementById("result").innerText = "Converted Time: " + converted;
+        let fromName = getSelectedText("fromZone");
+        let toName = getSelectedText("toZone");
+
+        document.getElementById("resultMessage").innerText = "Converesion Completed";
+
+        document.getElementById("fromName").innerText = fromName;
+        document.getElementById("toName").innerText = toName;
+        document.getElementById("originalTime").innerText = date + " " + time;
+        document.getElementById("convertedTime").innerText = converted;
+        document.getElementById("historyText").innerText = fromName + " to " + toName + " was converted recently.";
 }
